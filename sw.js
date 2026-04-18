102bfba774293f351da0734e2a908796cb6e42dc
nmainville <nmainville71@gmail.com>
2026-04-18 18:21:04 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'ops-hub-v1';
+
+// Install the new sponge
+self.addEventListener('install', event => {
+  self.skipWaiting(); 
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/flag.jpg',
+        '/bondsville.png',
+        '/redblueflag.png',
+        '/flame.png',
+        '/admin.png'
+      ]);
+    })
+  );
+});
+
+// WIPE THE OLD SPONGE when the version number changes
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch files
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request.url, fetchRes.clone());
+          return fetchRes;
+        });
+      });
+    }).catch(() => {
+      // Offline fallback
+    })
+  );
+});
