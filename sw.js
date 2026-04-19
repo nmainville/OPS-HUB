8836192fccfacc82f4842a496928e4c6196e948c
nmainville <nmainville71@gmail.com>
2026-04-19 12:54:58 -0400

    v64

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ops-hub-v62';
+const CACHE_NAME = 'ops-hub-v64';
 
 // INSTALL: Force the new worker to take over immediately (No waiting!)
 self.addEventListener('install', (event) => {
